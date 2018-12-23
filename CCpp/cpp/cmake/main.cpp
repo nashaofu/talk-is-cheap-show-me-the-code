250bd999206523382c5d44ac4c12b1ffce57b57d
nashaofu <diaocheng@outlook.com>
2018-12-23 22:13:00 +0800

    modify 编译文件

---
+++ CCpp/cpp/cmake/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "multiply/multiply.h"
+
+int main()
+{
+  std::cout << multiply(1, 2) << std::endl;
+  return 0;
+}
---
+++ CCpp/cpp/cmake/multiply/multiply.h
@@ -0,0 +1,6 @@
+#if !defined(MULTIPLY_H)
+#define MULTIPLY_H
+
+int multiply(int a, int b);
+
+#endif // MULTIPLY_H
