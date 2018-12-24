c9b8238968d3c988b0bd118e634fd25adf3fadf7
nashaofu <diaocheng@outlook.com>
2018-12-24 23:31:55 +0800

    gdb 支持

---
+++ CCpp/cpp/cmake/README.md
@@ -0,0 +1,7 @@
+# cmake使用教程
+
+1. 运行`ccmake .`，在交互式界面中选择参数值
+2. 按下`c`键保存配置，按下`g`键生成Makefile
+3. 执行`make`命令
+
+cmake教程文件[https://github.com/nashaofu/cmake-demo](https://github.com/nashaofu/cmake-demo)
--- CCpp/cpp/cmake/main.cpp
+++ CCpp/cpp/cmake/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include "math/multiply.h"
 #include "math/power.h"
+#include "config.h"
+
+#if defined(CMAKE_OPTION)
+void cmakeOption()
+{
+  std::cout << "cmakeOption" << std::endl;
+};
+#else
+void cmakeOption()
+{
+  std::cout << "cmakeOption2" << std::endl;
+};
+#endif
 
 int main()
 {
+  cmakeOption();
   std::cout << "multiply:" << multiply(123, 212) << std::endl;
   std::cout << "power:" << power(10, 2) << std::endl;
   return 0;
