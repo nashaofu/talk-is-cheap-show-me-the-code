b769ac600a98bad3d5d0937be7ae862f91c65ae3
nashaofu <diaocheng@outlook.com>
2020-03-03 21:30:41 +0800

    Create fps计算.js

---
+++ fps计算.js
@@ -0,0 +1,33 @@
+const div = document.createElement('div')
+document.body.appendChild(div)
+div.style.position = 'fixed'
+div.style.top = 0
+div.style.right = 0
+div.style.zIndex = 9999
+div.style.background = '#000'
+div.style.color = '#fff'
+div.style.padding = '20px'
+
+var frame = 0
+var lastTime = Date.now()
+var lastFameTime = Date.now()
+
+var loop = function () {
+  var now = Date.now()
+  var fs = (now - lastFameTime)
+  var fps = Math.round(1000 / fs)
+
+  lastFameTime = now
+  frame++
+
+  if (now > 1000 + lastTime) {
+    fps = Math.round((frame * 1000) / (now - lastTime))
+    div.innerHTML = `${fps}fps`
+    frame = 0
+    lastTime = now
+  };
+
+  requestAnimationFrame(loop)
+}
+
+loop()
